87dd60929c51eaf4580df6aeacfda4ea8cbd7109
Priyanshu Garg <priyanshu.g.garg@oracle.com>
2025-03-06 01:10:25 +0530

    Added new files LogMgr and LogIterator

--- src/main/java/simpledb/log/LogIterator.java
+++ src/main/java/simpledb/log/LogIterator.java
@@ -1,4 +1,48 @@
 package simpledb.log;
 
-public class LogIterator {
+import simpledb.file.Block;
+import simpledb.file.FileMgr;
+import simpledb.file.Page;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+class LogIterator implements Iterator<byte[]> {
+    private final FileMgr fileMgr;
+    private Block block;
+    private final Page page;
+    private int currentPosition;
+
+    public LogIterator(FileMgr fileMgr, Block block) throws IOException {
+        this.fileMgr = fileMgr;
+        this.block = block;
+        byte[] bytes = new byte[fileMgr.getBlocksize()];
+        page = new Page(bytes);
+        moveToBlock(block);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return (currentPosition < fileMgr.getBlocksize()) || (block.getBlockNumber() > 0);
+    }
+
+    @Override
+    public byte[] next() {
+        if (currentPosition == fileMgr.getBlocksize()) {
+            block = new Block(block.getFilename(), block.getBlockNumber() - 1);
+            try {
+                moveToBlock(block);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        byte[] record = page.getBytes(currentPosition);
+        currentPosition += (Integer.BYTES + record.length);
+        return record;
+    }
+
+    private void moveToBlock(Block block) throws IOException {
+        fileMgr.read(block, page);
+        currentPosition = page.getInt(0);
+    }
 }
--- src/main/java/simpledb/log/LogMgr.java
+++ src/main/java/simpledb/log/LogMgr.java
@@ -1,4 +1,73 @@
 package simpledb.log;
 
+import simpledb.file.Block;
+import simpledb.file.FileMgr;
+import simpledb.file.Page;
+
+import java.io.IOException;
+import java.util.Iterator;
+
 public class LogMgr {
+    private FileMgr fileMgr;
+    private String logFile;
+    private final Page logPage;
+    private Block currentBlock;
+    private int latestLSN;
+    private int lastSavedLSN;
+
+    public LogMgr(FileMgr fileMgr, String logFile) throws IOException {
+        this.latestLSN = 0;
+        this.lastSavedLSN = 0;
+        byte[] b = new byte[fileMgr.getBlocksize()];
+        this.logPage = new Page(b);
+        int logSize = fileMgr.length(logFile);
+        if (logSize == 0) {
+            currentBlock = appendNewBlock();
+        } else {
+            currentBlock = new Block(logFile, logSize - 1);
+            fileMgr.read(currentBlock, logPage);
+        }
+    }
+
+    public void flush(int lsn) {
+        if (lsn >= lastSavedLSN) {
+            flush();
+        }
+    }
+
+    public Iterator<byte[]> iterator() throws IOException {
+        flush();
+        return new LogIterator(fileMgr, currentBlock);
+    }
+
+    public synchronized int append(byte[] logRecord) {
+        int boundary = logPage.getInt(0);
+        int recordSize = logRecord.length;
+        int bytesNeeded = recordSize + Integer.BYTES;
+        if (boundary - bytesNeeded < Integer.BYTES) {
+            flush();
+            currentBlock = appendNewBlock();
+            boundary = logPage.getInt(0);
+        }
+
+        // filling the page from right to left
+        // in every iteration recordPosition becomes small to fulfil the condition
+        int recordPosition = boundary - bytesNeeded;
+        logPage.setBytes(recordPosition, logRecord);
+        logPage.setInt(0, recordPosition);
+        latestLSN += 1;
+        return latestLSN;
+    }
+
+    private Block appendNewBlock() {
+        Block block = fileMgr.append(logFile);
+        logPage.setInt(0, fileMgr.getBlocksize());
+        fileMgr.write(block, logPage);
+        return block;
+    }
+
+    private void flush() {
+        fileMgr.write(currentBlock, logPage);
+        lastSavedLSN = latestLSN;
+    }
 }
