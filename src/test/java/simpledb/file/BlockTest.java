98e7c6f4104f0c0204d3fad5dd2c31155b57ea4b
Priyanshu Garg <priyanshu.g.garg@oracle.com>
2025-03-09 00:53:35 +0530

    Added log and block test

--- src/test/java/simpledb/file/BlockTest.java
+++ src/test/java/simpledb/file/BlockTest.java
@@ -1,4 +1,7 @@
+package simpledb.file;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BlockTest {
-  
+
 }
--- src/test/java/simpledb/log/LogTest.java
+++ src/test/java/simpledb/log/LogTest.java
@@ -1,4 +1,9 @@
 package simpledb.log;
 
 public class LogTest {
+    private static LogMgr logMgr;
+
+    public static void main(String[] args) {
+
+    }
 }
