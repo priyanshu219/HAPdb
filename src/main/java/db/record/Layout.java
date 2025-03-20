581a7b8c69714fbcbbbc9f13451061eee211238d
Priyanshu Garg <priyanshu.g.garg@oracle.com>
2025-03-20 18:18:16 +0530

    Added Layout.java and Schema.java files for RecordPage

---
+++ src/main/java/db/record/Layout.java
@@ -0,0 +1,54 @@
+package db.record;
+
+import db.file.Page;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.sql.Types.INTEGER;
+import static java.sql.Types.VARCHAR;
+
+public class Layout {
+    private final Schema schema;
+    private final Map<String, Integer> offsets;
+    private final int slotSize;
+
+    public Layout(Schema schema) {
+        this.schema = schema;
+        this.offsets = new HashMap<>();
+        int pos = Integer.BYTES;
+        for (String fieldName : schema.getFields()) {
+            offsets.put(fieldName, pos);
+            pos += lengthInBytes(fieldName);
+        }
+
+        this.slotSize = pos;
+    }
+
+    public Layout(Schema schema, Map<String, Integer> offsets, int slotSize) {
+        this.schema = schema;
+        this.offsets = offsets;
+        this.slotSize = slotSize;
+    }
+
+    public Schema getSchema() {
+        return schema;
+    }
+
+    public int getFieldOffset(String fieldName) {
+        return offsets.get(fieldName);
+    }
+
+    public int getSlotSize() {
+        return slotSize;
+    }
+
+    private int lengthInBytes(String fieldName) {
+        int type = schema.getFieldType(fieldName);
+        if (type == INTEGER) {
+            return Integer.BYTES;
+        } else {
+            return Page.maxLength(schema.getFieldLength(fieldName));
+        }
+    }
+}
---
+++ src/main/java/db/record/Schema.java
@@ -0,0 +1,66 @@
+package db.record;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.sql.Types.INTEGER;
+import static java.sql.Types.VARCHAR;
+
+public class Schema {
+    private final List<String> fields;
+    private final Map<String, FieldInfo> infoMap;
+
+    public Schema() {
+        this.fields = new ArrayList<>();
+        this.infoMap = new HashMap<>();
+    }
+
+    public void addField(String fieldName, int type, int length) {
+        fields.add(fieldName);
+        infoMap.put(fieldName, new FieldInfo(type, length));
+    }
+
+    public void addIntField(String fieldName) {
+        addField(fieldName, INTEGER, 0);
+    }
+
+    public void addStringField(String fieldName, int length) {
+        addField(fieldName, VARCHAR, length);
+    }
+
+    public void addAll(Schema schema) {
+        for (String fieldName : schema.getFields()) {
+            add(fieldName, schema);
+        }
+    }
+
+    public void add(String fieldName, Schema schema) {
+        int type = schema.getFieldType(fieldName);
+        int length = schema.getFieldLength(fieldName);
+        addField(fieldName, type, length);
+    }
+
+    public List<String> getFields() {
+        return fields;
+    }
+
+    public int getFieldType(String fieldName) {
+        return infoMap.get(fieldName).type;
+    }
+
+    public int getFieldLength(String fieldName) {
+        return infoMap.get(fieldName).length;
+    }
+
+    private class FieldInfo {
+        int type;
+        int length;
+
+        public FieldInfo(int type, int length) {
+            this.type = type;
+            this.length = length;
+        }
+    }
+}
