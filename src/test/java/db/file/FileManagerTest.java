64c5ffbcc5a081649fba09c6c5bbd04d08f1fc9c
Priyanshu Garg <priyanshu.g.garg@oracle.com>
2025-03-21 20:08:02 +0530

    Added test file for fileManager

---
+++ src/test/java/db/file/FileManagerTest.java
@@ -0,0 +1,26 @@
+package db.file;
+
+import db.server.HAPdb;
+
+class FileManagerTest {
+    public static void main(String[] args) {
+        HAPdb db = new HAPdb("filetest", 400, 8);
+        FileManager fileManager = db.getFileManager();
+
+        Block block = new Block("testfile", 2);
+        Page page1 = new Page(fileManager.getBlockSize());
+        int position1 = 88;
+        String randomeString = "someRandomString";
+        page1.setString(position1, randomeString);
+        int stringSize = Page.maxLength(randomeString.length());
+        int position2 = position1 + stringSize;
+        page1.setInt(position2, 2109);
+        fileManager.write(block, page1);
+
+        Page page2 = new Page(fileManager.getBlockSize());x
+        fileManager.read(block, page2);
+
+        assert page2.getInt(position2) == 2109;
+        assert page2.getString(position1).equals(randomeString);
+    }
+}
