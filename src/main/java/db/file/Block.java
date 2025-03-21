5973482f82cef97a0c8940c917d4b4de4ed3ebbb
Priyanshu Garg <priyanshu.g.garg@oracle.com>
2025-03-21 20:07:31 +0530

    Removed redundant equals(), hashCode(), toString() from RID.java, Block.java

--- src/main/java/db/file/Block.java
+++ src/main/java/db/file/Block.java
@@ -1,22 +1,4 @@
 package db.file;
 
 public record Block(String fileName, int blockNumber) {
-
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof Block(String name, int number))) {
-            return false;
-        }
-        return fileName.equals(name) && blockNumber == number;
-    }
-
-    @Override
-    public String toString() {
-        return "[file " + fileName + ", block " + blockNumber + "]";
-    }
-
-    @Override
-    public int hashCode() {
-        return toString().hashCode();
-    }
 }
--- src/main/java/db/record/RID.java
+++ src/main/java/db/record/RID.java
@@ -1,20 +1,4 @@
 package db.record;
 
 public record RID(int blockNum, int slot) {
-
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof RID(int num, int slot1))) {
-            return false;
-        }
-        return (blockNum == num && slot == slot1);
-    }
-
-    @Override
-    public String toString() {
-        return "RID{" +
-                "blockNum=" + blockNum +
-                ", slot=" + slot +
-                '}';
-    }
 }
