80e7a91116ef248a77ad6fc96a611e4761cc3b9b
Priyanshu Garg <priyanshu.g.garg@oracle.com>
2025-03-22 10:45:03 +0530

    Test for Buffer

--- src/test/java/db/buffer/BufferTest.java
+++ src/test/java/db/buffer/BufferTest.java
@@ -4,8 +4,6 @@ import db.file.Block;
 import db.file.Page;
 import db.server.HAPdb;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class BufferTest {
     public static void main(String[] args) {
         HAPdb db = new HAPdb("buffertest", 400, 3);
@@ -14,5 +12,22 @@ class BufferTest {
         Buffer buffer1 = bufferManager.pin(new Block("testfile", 1));
         Page page = buffer1.getContents();
 
+        int intValue = page.getInt(80);
+        page.setInt(80, intValue + 1);
+        buffer1.setModified(1, 0);
+
+        bufferManager.unpin(buffer1);
+
+        // one of them replace buffer1 page, and flushed the changes
+        Buffer buffer = bufferManager.pin(new Block("testfile", 2));
+        bufferManager.pin(new Block("testfile", 3));
+        bufferManager.pin(new Block("testfile", 4));
+
+
+        assert buffer1.getBlock().blockNumber() != 1;
+
+        bufferManager.unpin(buffer);
+        Buffer buffer2 = bufferManager.pin(new Block("testfile", 1));
+        assert buffer2.getContents().getInt(80) == (intValue + 1);
     }
 }
