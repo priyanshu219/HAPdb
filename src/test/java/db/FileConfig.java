085a8961fee56e6f93cc5cdd82e52ad4387cb8a9
Priyanshu Garg <priyanshu.g.garg@oracle.com>
2025-03-22 14:08:37 +0530

    Added auxiliary classes for JUnit testing

---
+++ src/test/java/db/FileConfig.java
@@ -0,0 +1,54 @@
+package db;
+
+import db.buffer.BufferManager;
+import db.file.FileManager;
+import db.log.LogManager;
+import db.server.HAPdb;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileConfig implements BeforeAllCallback, AfterAllCallback {
+    private static HAPdb db;
+    private String directoryName;
+
+    public static HAPdb getDb() {
+        return db;
+    }
+
+    public static FileManager fileManager() {
+        return db.getFileManager();
+    }
+
+    public static LogManager logManager() {
+        return db.getLogManager();
+    }
+
+    public static BufferManager bufferManager() {
+        return db.getBufferManager();
+    }
+
+    @Override
+    public void beforeAll(ExtensionContext extensionContext) {
+        TestConfig config = extensionContext.getRequiredTestClass().getAnnotation(TestConfig.class);
+
+        directoryName = config.directoryName();
+        int blockSize = config.blockSize();
+        int totalBuffers = config.totalBuffers();
+
+        db = new HAPdb(directoryName, blockSize, totalBuffers);
+    }
+
+    @Override
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public void afterAll(ExtensionContext extensionContext) throws IOException {
+        File directory = new File(directoryName);
+        for (String fileName : directory.list()) {
+            new File(directory, fileName).delete();
+        }
+        directory.delete();
+    }
+}
---
+++ src/test/java/db/TestConfig.java
@@ -0,0 +1,14 @@
+package db;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface TestConfig {
+    String directoryName();
+    int blockSize();
+    int totalBuffers();
+}
