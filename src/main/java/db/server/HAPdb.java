cab575fa5546d82d4b7a0c7152c05463f2cdf8b0
priyanshu219 <priyanshugarg219@gmail.com>
2025-04-09 16:15:03 +0530

    Modified HAPdb to create transaction and planner.

--- src/main/java/db/server/HAPdb.java
+++ src/main/java/db/server/HAPdb.java
@@ -3,20 +3,35 @@ package db.server;
 import db.buffer.BufferManager;
 import db.file.FileManager;
 import db.log.LogManager;
+import db.metadata.MetadataManager;
+import db.planner.*;
+import db.transaction.Transaction;
 
 import java.io.File;
 
 public class HAPdb {
     public static final String LOG_FILE = "hapdb.log";
+    private static final int BLOCKSIZE = 800;
+    private static final int TOTALBUFFERS = 8;
     private final FileManager fileManager;
     private final LogManager logManager;
     private final BufferManager bufferManager;
+    private MetadataManager metadataManager;
 
     public HAPdb(String directoryName, int blockSize, int totalBuffers) {
         File dbDirectory = new File(directoryName);
         this.fileManager = new FileManager(dbDirectory, blockSize);
         this.logManager = new LogManager(fileManager, LOG_FILE);
         this.bufferManager = new BufferManager(fileManager, logManager, totalBuffers);
+    }
+
+    public HAPdb(String directoryName) {
+        this(directoryName, BLOCKSIZE, TOTALBUFFERS);
+        Transaction transaction = getNewTransaction();
+
+        boolean isNew = fileManager.isNew();
+        this.metadataManager = new MetadataManager(isNew, transaction);
+        transaction.commit();
     }
 
     public BufferManager getBufferManager() {
@@ -31,4 +46,13 @@ public class HAPdb {
         return fileManager;
     }
 
+    public Transaction getNewTransaction() {
+        return new Transaction(fileManager, logManager, bufferManager);
+    }
+
+    public Planner getPlanner() {
+        QueryPlanner queryPlanner = new BasicQueryPlanner(metadataManager);
+        UpdatePlanner updatePlanner = new BasicUpdatePlanner(metadataManager);
+        return new Planner(queryPlanner, updatePlanner);
+    }
 }
