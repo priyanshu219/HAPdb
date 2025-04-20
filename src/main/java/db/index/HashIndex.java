4106dceeae37ecd84b4c5fc094613563b84e3ab0
priyanshu219 <priyanshugarg219@gmail.com>
2025-04-20 13:35:08 +0530

    Added HashIndex for storing records in an index on the basis of hash function.

--- src/main/java/db/index/HashIndex.java
+++ src/main/java/db/index/HashIndex.java
@@ -3,43 +3,82 @@ package db.index;
 import db.query.Constant;
 import db.record.Layout;
 import db.record.RID;
+import db.record.TableScan;
 import db.transaction.Transaction;
 
 public class HashIndex implements Index {
-    public HashIndex(Transaction transaction, String indexName, Layout indexLayout) {
+    private static final int NUM_BUCKETS = 100;
+    private final Transaction transaction;
+    private final String indexName;
+    private final Layout layout;
+    private Constant searchKey;
+    private TableScan scan;
+
+    public HashIndex(Transaction transaction, String indexName, Layout layout) {
+        this.transaction = transaction;
+        this.indexName = indexName;
+        this.layout = layout;
+
+        this.searchKey = null;
+        this.scan = null;
     }
 
     public static int searchCost(int numBlocks, int rpd) {
-        return 0;
+        return numBlocks / HashIndex.NUM_BUCKETS;
     }
 
     @Override
     public void beforeFirst(Constant searchKey) {
-
+        close();
+        this.searchKey = searchKey;
+        int bucket = searchKey.hashCode() % NUM_BUCKETS;
+        String tableName = indexName + bucket;
+        scan = new TableScan(transaction, tableName, layout);
     }
 
     @Override
     public boolean next() {
+        while (scan.next()) {
+            if (scan.getValue("dataVal").equals(searchKey)) {
+                return true;
+            }
+        }
+
         return false;
     }
 
     @Override
     public RID getDataRid() {
-        return null;
+        int blockNum = scan.getInt("block");
+        int id = scan.getInt("id");
+
+        return new RID(blockNum, id);
     }
 
     @Override
     public void insert(Constant dataVal, RID dataRID) {
-
+        beforeFirst(searchKey);
+        scan.insert();
+        scan.setInt("block", dataRID.blockNum());
+        scan.setInt("id", dataRID.blockNum());
+        scan.setValue("dataVal", dataVal);
     }
 
     @Override
     public void delete(Constant dataVal, RID dataRID) {
-
+        beforeFirst(searchKey);
+        while (next()) {
+            if (getDataRid().equals(dataRID)) {
+                scan.delete();
+                return;
+            }
+        }
     }
 
     @Override
     public void close() {
-
+        if (null != scan) {
+            scan.close();
+        }
     }
 }
